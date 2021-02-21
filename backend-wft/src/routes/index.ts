8e0fd9a8de161853f94c550f22b58e4eed559f82
nicolo.frison <nicolo.frison97@gmail.com>
2021-02-21 22:58:20 +0100

    Added messages feature

--- backend-wft/src/routes/index.ts
+++ backend-wft/src/routes/index.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import { Server } from "socket.io";
 import FileController from "../controllers/file.controller";
+import MessageController from "../controllers/message.controller";
 
 const router = express.Router();
 
 let routes = (app: express.Application, io: Server) => {
   const fileController = new FileController(io);
+  const messageController = new MessageController(io);
 
   router.post("/upload", fileController.upload);
   router.get("/files", fileController.getListFiles);
   router.get("/files/:name", fileController.download);
+
+  router.get("/messages", messageController.getMessages);
+  router.post("/messages", messageController.postMessages)
 
   app.use(router);
 };
